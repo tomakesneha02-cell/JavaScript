96f1e6ef077bfdc659fba5236c3412fbc54938c3
tomakesneha02-cell <tomakesneha02@gmail.com>
2026-08-29 07:30:26 +0000

    container config Done

---
+++ 01_Basics/variables.js
@@ -0,0 +1,14 @@
+const accountid=144553
+let accountEmail="snehatomake02@gmail.com"
+var accountPassword ="12345"
+accountcity ="Jaipur"
+
+// accountid=2//not allowed
+
+accountEmail="hc@hc.com"
+accountPassword="1212"
+
+accountcity="Bangaluru"
+console.log(accountid);
+
+console.table([accountEmail,accountid,accountPassword,accountcity])
