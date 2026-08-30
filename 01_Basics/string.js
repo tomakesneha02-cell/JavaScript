4dd6c14d5045ab284cd7bd785b8bb15971a2b4f5
tomakesneha02-cell <tomakesneha02@gmail.com>
2026-08-30 11:57:17 +0000

    Container Config Done

---
+++ 01_Basics/string.js
@@ -0,0 +1,15 @@
+// String Methods
+ let name="sneha"
+ console.log(name.length)
+ console.log(name.toUpperCase());
+ console.log(name.toLowerCase());
+
+ //Slice-extracting the part of string with strat and end and end always -1
+
+ console.log(name.slice(0,4));
+ 
+ //substring: it also same extracting part of string  with start and end 
+console.log("substring",name.substring(0,4));
+
+
+ 
