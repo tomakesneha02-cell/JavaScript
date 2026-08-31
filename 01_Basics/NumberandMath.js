1a5b882d29e004b75472157d7f2e6b8ea07614aa
tomakesneha02-cell <tomakesneha02@gmail.com>
2026-08-31 10:28:05 +0000

    config done

--- 01_Basics/NumberandMath.js
+++ 01_Basics/NumberandMath.js
@@ -27,4 +27,11 @@ console.log(hund.toLocaleString());
 console.log(Math.abs(-4));
 console.log(Math.round(4.6))
 console.log(Math.ceil(4.2))
+console.log(Math.floor(4.3))
+
+console.log(Math.min(1,2,3))
+console.log(Math.max(1,2,3))
+
+console.log(Math.random(1,10))
+
   
