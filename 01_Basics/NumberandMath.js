aacb0d9ac65f1cc74f221e026a960e19b78deef1
tomakesneha02-cell <tomakesneha02@gmail.com>
2026-08-31 10:10:58 +0000

    Container Config Done

---
+++ 01_Basics/NumberandMath.js
@@ -0,0 +1,30 @@
+ const score=100;
+ console.log(score);
+
+ const ob1= new Number(200)
+ console.log(ob1);
+
+ console.log(typeof(ob1));
+ console.log(ob1.toFixed(1));
+
+
+//toPrecision() is a JavaScript method used to format a number
+//tells us how many total significant digits we want.”
+ const othernumber=123.8999
+
+ console.log(othernumber.toPrecision(4))
+
+
+//toLocalString()
+
+const hund=10000
+console.log(hund.toLocaleString());
+
+
+ //---------------- Maths ---------------------
+
+ console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.2))
+  
--- 01_Basics/string.js
+++ 01_Basics/string.js
@@ -9,7 +9,51 @@
  console.log(name.slice(0,4));
  
  //substring: it also same extracting part of string  with start and end 
-console.log("substring",name.substring(0,4));
+console.log("substring :--",name.substring(0,5));
 
+//substr- start ,length
+
+console.log(name.substr(0,1));
 
  
+//concat()-  joining the strings
+
+let Fname="Sneha"
+let Lname="Tomake"
+
+console.log("Concate:--",Fname+Lname);
+
+//trim: remove extra space from the memory 
+
+let Trimtest1="               Hello            "
+let Trimtest2="                Sneha           "
+console.log(Trimtest1);
+console.log(Trimtest2);
+console.log(Trimtest1.trim());
+
+console.log(Trimtest2.trimStart())
+console.log(Trimtest2.trimEnd());
+
+//padstart:-- adding char or we can say value at the end or start 
+//sneha
+let padTest ="e"
+console.log(padTest.padStart(2,"n"));
+
+let number = "5";
+
+console.log(number.padStart(3, "0"));
+
+console.log(number.repeat(3));
+
+
+//Replace
+
+let replacetest="i learn java"
+console.log(replacetest.replace("java","javascript"));
+
+
+//charAt()
+
+console.log(replacetest.charAt(0));
+
+
