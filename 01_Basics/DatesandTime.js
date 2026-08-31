74c1f9e328839c6b3b83db6dfd94dcbb105cb77c
tomakesneha02-cell <tomakesneha02@gmail.com>
2026-08-31 11:11:33 +0000

    config done

---
+++ 01_Basics/DatesandTime.js
@@ -0,0 +1,19 @@
+//Dates
+
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toString());
+console.log(myDate.getDate());
+console.log(myDate.getDay());
+console.log(myDate.getFullYear());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+
+console.log(typeof(myDate));
+let myCreatedDate = new Date(2023, 11 ,23)
+console.log(myCreatedDate.toDateString());
+
+
+
+
