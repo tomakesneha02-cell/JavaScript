8e25293e5303ddd7d8087877f80cc9a3927bbb3b
tomakesneha02-cell <tomakesneha02@gmail.com>
2026-09-04 09:12:01 +0000

    done

--- 02_Basics/objectpart1.js
+++ 02_Basics/objectpart1.js
@@ -18,3 +18,61 @@ console.log(jsuder["email"]);
 console.log(jsuder["fname"]);
 
 // symbol:-- Symbol is a primitive data type used to create unique identifiers/property keys in JavaScript.
+ 
+const a=Symbol("id")
+const b=Symbol("id")
+
+console.log(a===b)
+
+
+const id= Symbol("id")
+const user={
+    name:"sneha",
+    [id]:101
+};
+console.log(user[id])
+
+
+//non single tone
+
+// const tinderUser ={}
+
+// tinderUser.id = "123abc"
+// tinderUser.name="sneha"
+// tinderUser.islogged=false
+
+// console.log("tinderuser data:--",tinderUser)
+
+
+const regularuser={
+    name : "sneha ",
+    fullname:{
+        username:{
+            fname:"sneha",
+            lname:"tomake"
+        }
+    }
+}
+
+console.log(regularuser.fullname);
+console.log(regularuser.fullname.username);
+
+//object de-structued and json  
+
+
+console.log("-------------------------------------");
+
+const course={
+    cname:"js in hindi",
+    price:"999",
+    coursIntructor:"sneha"
+}
+
+// course.coursIntructor
+
+const {cname:coursname}=course
+
+// console.log(cname); give error 
+
+console.log(coursname)
+
